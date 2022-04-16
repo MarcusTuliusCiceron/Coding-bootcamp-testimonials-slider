8acbc8775c5c100bbec9d4c01ee0fdc85b5c24e9
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-04-16 11:46:04 +0200

    challenge complete

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,23 @@
+const prevBtn = document.querySelector('.arrow_left')
+const nextBtn = document.querySelector('.arrow_right')
+const quote1 = document.querySelector('.quote1')
+const portrait1 = document.querySelector('.portrait1')
+const quote2 = document.querySelector('.quote2')
+const portrait2 = document.querySelector('.portrait2')
 
 
+prevBtn.addEventListener("click", ()=>{
+    console.log('click')
+    quote1.classList.toggle("active")
+    portrait1.classList.toggle("active")
+    quote2.classList.toggle("active")
+    portrait2.classList.toggle("active")
+})
 
+nextBtn.addEventListener("click", ()=>{
+    console.log('click')
+    quote1.classList.toggle("active")
+    portrait1.classList.toggle("active")
+    quote2.classList.toggle("active")
+    portrait2.classList.toggle("active")
+})
